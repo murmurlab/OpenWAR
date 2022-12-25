67aa48830c16f19ea9872e8af8bc0bd01f28bee1
murmur <0aeon0@mail.com>
2022-12-25 18:49:42 +0300

    Update libft_testester_By_NaN0BiT.sh

--- libft_testester_By_NaN0BiT.sh
+++ libft_testester_By_NaN0BiT.sh
@@ -3,7 +3,7 @@ counter=1
 while [ $counter -le 9999 ]
 do
         ((counter++))
-	osascript -e "set Volume 1"
+	osascript -e "set Volume 6"
        # say -v xander "otuzbir"
        afplay ~/libft.h
        sleep 100
